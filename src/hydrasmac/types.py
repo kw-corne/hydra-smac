c06e4ea1143959e8f365ae757fbca927898db9b1
c <corne.spek15@gmail.com>
2023-03-10 15:20:50 +0100

    make incumbent(s) a dataclass

--- src/hydrasmac/types.py
+++ src/hydrasmac/types.py
@@ -1,8 +1,6 @@
 from typing import Callable
 
 from ConfigSpace import Configuration
-from smac.runhistory.runhistory import RunHistory
 
 CostDict = dict[str, float]
-Incumbent = tuple[Configuration, RunHistory, CostDict]
 TargetFunction = Callable[[Configuration, str, int], float]
