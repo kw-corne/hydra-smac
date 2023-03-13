3cbed5fb99a9da9fbbe415761946d2a9e5840eb3
c <corne.spek15@gmail.com>
2023-03-13 13:21:31 +0100

    use fixtures in scen util tests

--- tests/test_scenario_util.py
+++ tests/test_scenario_util.py
@@ -1,45 +1,27 @@
 from pathlib import Path
 
-from ConfigSpace import ConfigurationSpace, Float
+from ConfigSpace import Configuration
 from smac.scenario import Scenario
 
 import src.util.scenario_util as scenario_util
 
-DummyConfig = ConfigurationSpace()
-DummyConfig.add_hyperparameters(
-    [
-        Float("x", (1.0, 5.0)),
-        Float("y", (1.0, 5.0)),
-        Float("z", (1.0, 5.0)),
-    ]
-)
 
-instances = ["a", "b", "c"]
-instance_features = {"a": [0.0], "b": [0.0], "c": [0.0]}
-
-DummyScenario = Scenario(
-    DummyConfig,
-    instances=instances,
-    instance_features=instance_features,
-)
-
-
-def test_get_scenario_dict():
-    scenario_dict = scenario_util.get_scenario_dict(DummyScenario)
+def test_get_scenario_dict(scenario: Scenario, config_space: Configuration):
+    scenario_dict = scenario_util.get_scenario_dict(scenario)
 
     # (TODO) Comparing the most important keys here, perhaps add a check for
     # all relevant keys later?
-    assert scenario_dict["configspace"] == DummyConfig
-    assert scenario_dict["instances"] == DummyScenario.instances
-    assert scenario_dict["instance_features"] == DummyScenario.instance_features
+    assert scenario_dict["configspace"] == config_space
+    assert scenario_dict["instances"] == scenario.instances
+    assert scenario_dict["instance_features"] == scenario.instance_features
 
 
-def test_set_scenario_output_dir():
+def test_set_scenario_output_dir(scenario: Scenario):
     output_dir = Path("./test_dir")  # TODO
     run_name = "test_run"
 
     scenario = scenario_util.set_scenario_output_dir(
-        DummyScenario, output_dir, run_name
+        scenario, output_dir, run_name
     )
 
     assert scenario.output_directory == output_dir / run_name / str(
@@ -48,13 +30,14 @@ def test_set_scenario_output_dir():
     assert scenario.name == run_name
 
 
-def test_set_scenario_instances():
-    dummy_inst = ["hello", "world"]
-    dummy_inst_feat = {"hello": [2.0], "world": [10.0]}
-
+def test_set_scenario_instances(
+    scenario: Scenario,
+    instances: list[str],
+    instance_features: dict[str, list[float]],
+):
     scenario = scenario_util.set_scenario_instances(
-        DummyScenario, dummy_inst, dummy_inst_feat
+        scenario, instances, instance_features
     )
 
-    assert scenario.instances == dummy_inst
-    assert scenario.instance_features == dummy_inst_feat
+    assert scenario.instances == instances
+    assert scenario.instance_features == instance_features
